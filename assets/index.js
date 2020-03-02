ef1715ff0c6b8411616447b6a779c2edbebc3610
umaiibo <j.hnpn123@gmail.com>
2020-03-01 23:37:22 -0800

    created event listener for the generate button as well as create an output value for the range slider

--- assets/index.js
+++ assets/index.js
@@ -2,12 +2,14 @@
 var lowercaseSelect = document.getElementById("lowercase");
 var uppercaseSelect = document.getElementById("uppercase");
 var numberSelect = document.getElementById("numbers");
-var specialSelect = document.getElementById("specials");
+var specialSelect = document.getElementById("special");
+var passLength = document.getElementById("passlength");
 var passField = document.getElementById("pass-field");
-var generateButton = document.getElementById("generate-button");
+var generateButton = document.getElementById("generate");
 var copyButton = document.getElementById("copy-button");
 
-// these functions will randomly pull out what ever option we choose
+// these functions will be a setup for when we randomly pull out 
+// what ever option we choose
 function randomLower() {
     var lowercase = "abcdefghijklmnopqrstuvwxyz";
     return lowercase[Math.floor(Math.random() * lowercase.length)];
@@ -28,5 +30,31 @@ function randomSpecial() {
     return specials[Math.floor(Math.random() * specials.length)];
 }
 
+// this is will include all those functions into one
+var randomSelect = {
+    lowercase: randomLower,
+    uppercase: randomUpper,
+    numbers: randomNumber,
+    specials: randomSpecial,
+}
 
-console.log(randomSpecial());
+// for the range slider
+output = passLength.value;
+
+passLength.oninput = function() {
+    output = this.value;
+    console.log(output);
+}
+
+// we need the event for the generateButton to function
+generateButton.addEventListener("click", function() {
+    var length = +passLength.value;
+    var isLower = lowercaseSelect.checked;
+    var isUpper = uppercaseSelect.checked;
+    var isNumber = numberSelect.checked;
+    var isSpecial = specialSelect.checked;
+
+    console.log(isLower, isUpper, isNumber, isSpecial, length); //this will ensure if the checkboxes are working
+
+    passField.innerText = generatePass(isLower, isUpper, isNumber, isSpecial, length);
+});
